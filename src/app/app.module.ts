a29aeba47321d822fc90d51b8c6fe9f21f783dfd
Wambita Fana <wambitafana@gmail.com>
2019-04-01 12:37:42 +0300

    Add the directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -26,7 +25,7 @@ import { HighlightDirective } from './directives/highlight.directive';
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
-    NgbModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
